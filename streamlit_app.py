ea4179ecd4b407292b618d5f734821bf8ce24b0c
Yinhao Deng <43745876+YinhaoDeng@users.noreply.github.com>
2023-07-07 14:32:41 +0930

    Update streamlit_app.py
    read text menu list using pandas

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,8 @@
 import streamlit
+import pandas
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt") #We want pandas to read our CSV file from that S3 bucket so we use a pandas function called read_csv  to pull the data into a dataframe we'll call my_fruit_list. 
+
 
 streamlit.title("My Parents New Healthy Dinner")
 
@@ -6,4 +10,6 @@ streamlit.header("🥣 Breakfast Menu")
 streamlit.text("🥗 Omega 3 & Blueberry Oatmeal")
 streamlit.text("🐔 Kale, Spinach & Rocket Smoothie")
 streamlit.text("🥑 🍞 Hard-Boiled Free-Range Egg")
+
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+streamlit.dataframe(my_fruit_list) # After pulling the data into a pandas dataframe called my_fruit_list, we will ask the streamlit library to display it on the page by typing:
